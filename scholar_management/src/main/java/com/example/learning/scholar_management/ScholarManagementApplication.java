b31fb426129e8fdb06ed651755dd01f30119c284
Arul Sivaraj G <149382430+ArulSivaraj@users.noreply.github.com>
2026-09-10 05:37:49 +0000

    initial commit

---
+++ scholar_management/src/main/java/com/example/learning/scholar_management/ScholarManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning.scholar_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarManagementApplication.class, args);
+	}
+
+}
---
+++ scholar_management/src/test/java/com/example/learning/scholar_management/ScholarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning.scholar_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
